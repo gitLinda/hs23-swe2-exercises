69a88a2e116a2ea8f9df94b3011f701779e42620
lkrue <lkrueger17@gmail.com>
2023-09-14 20:12:56 +0200

    equals and hashCode exercise

--- src/main/java/ch/juventus/object/Application.java
+++ src/main/java/ch/juventus/object/Application.java
@@ -12,7 +12,15 @@ public class Application {
             false
         );
 
-        System.out.println(linda);
+        Person lindaCopy = new Person(
+                "linda",
+                "krueger",
+                new Address("heimstrasse", 8, 8952, "dietikon"),
+                31,
+                false
+        );
+
+        System.out.println(linda.equals(lindaCopy));
 
     }
 
